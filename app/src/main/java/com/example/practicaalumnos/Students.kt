55984ad73c1baef2b7423a016781fc7e88a3f2b5
Roberto <robertogarciagarcia@gmail.com>
2022-12-02 14:34:00 +0100

    Ultimo commit

--- app/src/main/java/com/example/practicaalumnos/Students.kt
+++ app/src/main/java/com/example/practicaalumnos/Students.kt
@@ -4,11 +4,13 @@ import java.io.Serializable
 
 data class Students(
 
+    var idStudents: Int,
     var idImage: Int,
     var idName: String,
     var idSurname: String,
-    var idNote: String,
+    var idNote: Int,
     var idGrade: String,
     var idAge: Int
+
 
 ) : Serializable
